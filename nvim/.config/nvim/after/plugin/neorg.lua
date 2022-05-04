e032c8df606bda94a492c10688688cbb274c6fb5
engeir <eirroleng@gmail.com>
2022-05-04 14:47:45 +0200

    Add neorg config

--- nvim/.config/nvim/after/plugin/neorg.lua
+++ nvim/.config/nvim/after/plugin/neorg.lua
@@ -4,5 +4,22 @@ if not ok then
 end
 
 neorg.setup({
-    ...,
+    load = {
+        ["core.defaults"] = {},
+        ["core.norg.concealer"] = {
+            config = { -- Note that this table is optional and doesn't need to be provided
+                -- Configuration here
+            },
+        },
+        ["core.norg.completion"] = {
+            config = {
+                engine = "nvim-cmp",
+            },
+        },
+        ["core.integrations.treesitter"] = {
+            config = { -- Note that this table is optional and doesn't need to be provided
+                -- Configuration here
+            },
+        },
+    },
 })
