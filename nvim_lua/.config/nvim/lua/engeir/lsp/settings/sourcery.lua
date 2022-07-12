07538b11826a84bd432592eeb56f8df1913ce482
engeir <engeir@pm.me>
2022-07-12 08:56:57 -0600

    Add check to see if `pass` cmd is available

--- nvim_lua/.config/nvim/lua/engeir/lsp/settings/sourcery.lua
+++ nvim_lua/.config/nvim/lua/engeir/lsp/settings/sourcery.lua
@@ -1,3 +1,9 @@
+local executable = function(x)
+    return vim.fn.executable(x) == 1
+end
+if not executable("pass") then
+    return
+end
 return {
     init_options = {
         token = io.popen("pass Sourcery/token", "r"):read("l"),
