490d555cd3a2d93066a55aa3a8808ef050307464
engeir <engeir@pm.me>
2022-08-16 17:00:29 -0600

    feat: add keymaps to aerial

--- nvim_lua/.config/nvim/lua/engeir/aerial.lua
+++ nvim_lua/.config/nvim/lua/engeir/aerial.lua
@@ -14,10 +14,10 @@ aerial.setup({
     -- Useful for setting keymaps. Takes a single `bufnr` argument.
     on_attach = function(bufnr)
         -- Toggle the aerial window with <leader>a
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>at", "<cmd>AerialToggle!<CR>", {})
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>at", "<cmd>AerialToggle<CR>", {})
         -- Jump forwards/backwards with '{' and '}'
-        -- vim.api.nvim_buf_set_keymap(bufnr, "n", "{", "<cmd>AerialPrev<CR>", {})
-        -- vim.api.nvim_buf_set_keymap(bufnr, "n", "}", "<cmd>AerialNext<CR>", {})
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ap", "<cmd>AerialPrev<CR>", {})
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>an", "<cmd>AerialNext<CR>", {})
         -- -- Jump up the tree with '[[' or ']]'
         -- vim.api.nvim_buf_set_keymap(bufnr, "n", "[[", "<cmd>AerialPrevUp<CR>", {})
         -- vim.api.nvim_buf_set_keymap(bufnr, "n", "]]", "<cmd>AerialNextUp<CR>", {})
@@ -27,5 +27,5 @@ aerial.setup({
     close_on_select = true,
 
     -- Show box drawing characters for the tree hierarchy
-    show_guides = false,
+    show_guides = true,
 })
