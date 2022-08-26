2a1bb1e15886255b356d3e0f170948c6d9c23400
engeir <engeir@pm.me>
2022-08-26 10:49:18 -0600

    feat: add simple search engine to tmux using oi
    See source code to `oi` [here](https://github.com/PureArtistry/oi)

---
+++ bin/bin/tmux-search.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -r -p "Enter Search: " search
+
+if ! command -v oi &>/dev/null; then
+    # If this script is run with `tmux neww ...`
+    # tmux neww bash -c "echo 'Command oi is not in PATH' | less"
+    # If this script is run with `tmux popup ...`
+    echo 'Command oi is not in PATH'
+else
+    # If this script is run with `tmux neww ...`
+    # tmux neww bash -c "oi $search | less"
+    # tmux popup "oi $search"
+    # If this script is run with `tmux popup ...`
+    oi "$search"
+fi
