1d838eeac462343ca10131e65370eb812bc94fe4
engeir <engeir@pm.me>
2022-09-19 15:41:04 -0600

    style: improve formatting of inserted HTML code

--- bin/bin/revealjs-make-reflist.py
+++ bin/bin/revealjs-make-reflist.py
@@ -7,20 +7,23 @@ except IndexError:
     # If input comes from stdin, we are creating the final HTML code that goes into the
     # slide, therefore we start with ## References, and split up on every fourth
     # reference
-    print("## References")
+    print("## References\n")
     soup = BeautifulSoup(sys.stdin, features="lxml")
     div = soup.find("div", id="refs")
     d_list = div.contents
     # All even-indexed elements are newline characters
     del d_list[::2]
-    for i, element in enumerate(d_list):
-        if i > 0 and i % 4 == 0:
-            print("--")
+    new_page = False
+    i = 0
+    wc = 0
+    for element in d_list:
+        wc += len(element.get_text())
+        if wc > 1000 or (i > 0 and i % 4 == 0):
+            print("\n--\n")
+            i += 1
+            wc = 0
         print(element)
         print('<!-- .element: style="font-size:20pt" -->')
-    # print('\n<!-- .element: style="font-size:20pt" -->\n'.join(map(str, d_list)))
-    # Join only adds the string in between, so we need one extra at the end
-    # print('<!-- .element: style="font-size:20pt" -->')
 else:
     with open(file, "r") as fp:
         soup = BeautifulSoup(fp, features="lxml")
