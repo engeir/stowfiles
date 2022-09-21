12568bbf5def9c2a22736718dbd249a5388fb6a8
engeir <engeir@pm.me>
2022-09-21 09:09:03 -0600

    fix: actually don't think this fixes dial.nvim
    I do not understand how dial.nvim is supposed to work/be configured

--- nvim_lua/.config/nvim/lua/engeir/dial-nvim.lua
+++ nvim_lua/.config/nvim/lua/engeir/dial-nvim.lua
@@ -1,11 +1,27 @@
+-- I don't think this works at all...
+
 local ok, dial = pcall(require, "dial.map")
 if not ok then
     return
 end
+local ok2, augend = pcall(require, "dial.augend")
+if not ok2 then
+    return
+end
 
-vim.api.nvim_set_keymap("n", "<C-a>", dial.inc_normal(), {noremap = true})
-vim.api.nvim_set_keymap("n", "<C-x>", dial.dec_normal(), {noremap = true})
-vim.api.nvim_set_keymap("v", "<C-a>", dial.inc_visual(), {noremap = true})
-vim.api.nvim_set_keymap("v", "<C-x>", dial.dec_visual(), {noremap = true})
-vim.api.nvim_set_keymap("v", "g<C-a>", dial.inc_gvisual(), {noremap = true})
-vim.api.nvim_set_keymap("v", "g<C-x>", dial.dec_gvisual(), {noremap = true})
+require("dial.config").augends:register_group({
+    default = {
+        augend.constant.alias.en_weekday,
+    },
+})
+
+local keymap = vim.keymap.set
+
+keymap("n", "<C-a>", dial.inc_normal(), { noremap = true })
+keymap("n", "<C-x>", dial.dec_normal(), { noremap = true })
+keymap("v", "<C-a>", dial.inc_visual(), { noremap = true })
+keymap("v", "<C-x>", dial.dec_visual(), { noremap = true })
+keymap("v", "g<C-a>", dial.inc_gvisual(), { noremap = true })
+keymap("v", "g<C-x>", dial.dec_gvisual(), { noremap = true })
+
+-- 2022-12-31
