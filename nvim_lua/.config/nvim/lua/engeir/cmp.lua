889048e8388b156c16ff8359e27d5cce40007727
engeir <engeir@pm.me>
2022-10-24 14:45:17 -0600

    feat: remove smartyank and add swenv

--- nvim_lua/.config/nvim/lua/engeir/cmp.lua
+++ nvim_lua/.config/nvim/lua/engeir/cmp.lua
@@ -123,7 +123,7 @@ cmp.setup({
 
 -- Setup lspconfig.
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 -- require("lspconfig")["pyright, rust_analyzer, tsserver"].setup({
 --     capabilities = capabilities,
--- nvim_lua/.config/nvim/lua/engeir/plugins.lua
+++ nvim_lua/.config/nvim/lua/engeir/plugins.lua
@@ -97,7 +97,7 @@ return packer.startup(function(use)
     use("samodostal/image.nvim")
 
     -- General text manipulation and fonts ========================================== --
-    use({ "ibhagwan/smartyank.nvim" })
+    -- use({ "ibhagwan/smartyank.nvim" })
     use("numToStr/Comment.nvim")
     use({
         "kylechui/nvim-surround",
@@ -216,6 +216,8 @@ return packer.startup(function(use)
     end
     -- Latex
     use("lervag/vimtex")
+    -- Python venv
+    use("AckslD/swenv.nvim")
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
