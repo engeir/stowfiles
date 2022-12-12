354be4add474c10d449a430c51115c784e64faa5
Eirik Rolland Enger <eirroleng@gmail.com>
2022-12-11 22:46:19 -0700

    chore: clean up keymaps
    Cleans up the global and almost global keymaps in `keymaps.lua` and for
    the LSP.

--- nvim_lua/.config/nvim/lua/engeir/lsp/init.lua
+++ nvim_lua/.config/nvim/lua/engeir/lsp/init.lua
@@ -7,77 +7,64 @@ end
 
 require("engeir.lsp.null-ls")
 lsp.preset("recommended")
+lsp.set_preferences({
+    set_lsp_keymaps = false,
+})
 lsp.nvim_workspace()
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
--- lsp.on_attach(function(client, bufnr)
---     local opts = { buffer = bufnr, remap = false }
---     local keymap = vim.keymap.set
---     -- keymap("n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
---     keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
---     -- keymap("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
---     keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
---     keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
---     keymap("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
---     -- keymap("n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
---     keymap("i", "<C-h>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
---     -- keymap("n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
---     keymap("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
---     keymap("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
---     keymap("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
---     keymap("n", "<leader>vws", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>", opts)
---     keymap("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
---     keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
---     -- keymap("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
---     -- keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
---     -- keymap("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
---     keymap("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
---     -- keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
---     keymap("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
---     -- keymap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
---     keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
---     -- keymap("n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
---     keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
---     -- keymap("n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", opts)
---     keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
---     -- keymap("n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
---     keymap("n", "<leader>s", "<cmd>lua vim.lsp.buf.format({ async=true})<CR>", opts)
---     -- keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting()<cr>", opts)
---     keymap("n", "<leader>li", "<cmd>LspInfo<cr>", opts)
---     keymap("n", "<leader>lI", "<cmd>LspInstallInfo<cr>", opts)
--- end)
 lsp.on_attach(function(_, bufnr)
-    -- NOTE: Remember that lua is a real programming language, and as such it is possible
-    -- to define small helper and utility functions so you don't have to repeat yourself
-    -- many times.
-    --
-    -- In this case, we create a function that lets us more easily define mappings specific
-    -- for LSP related items. It sets the mode, buffer and description for us each time.
-    local nmap = function(keys, func, desc)
+    -- NOTE: Remember that lua is a real programming language, and as such it is
+    -- possible to define small helper and utility functions so you don't have to repeat
+    -- yourself many times.
+    -- In this case, we create a function that lets us more easily define mappings
+    -- specific for LSP related items. It sets the mode, buffer and description for us
+    -- each time.
+    local map = function(mode, keys, func, desc)
         if desc then
             desc = "LSP: " .. desc
         end
-
-        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+        vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = desc })
     end
+    local nmap = function(keys, func, desc)
+        map("n", keys, func, desc)
+    end
+    local imap = function(keys, func, desc)
+        map("i", keys, func, desc)
+    end
+    local xmap = function(keys, func, desc)
+        map("x", keys, func, desc)
+    end
+
+    -- Keymaps inspired by kickstart.nvim + lsp-zero
+    -- All default mapping set by lsp-zero:
+    -- https://github.com/VonHeikemen/lsp-zero.nvim/issues/15#issuecomment-1128842548
 
     nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
     nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+    xmap("<leader>ca", "<cmd>lua vim.lsp.buf.range_code_action()<cr>", "Range [C]ode [A]ction")
 
     nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-    nmap("<leader>gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
     nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+    -- Telescope alternative to vim.lsp.buf.references
+    nmap("gt", vim.lsp.buf.type_definition, "[G]oto [T]ype Definition")
+    nmap("<leader>gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
     nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
     nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+    -- Diagnostics
+    nmap("gl", vim.diagnostic.open_float, "Open Float Diagnostics")
+    nmap("[d", vim.diagnostic.goto_prev, "Goto Next Diagnostics")
+    nmap("]d", vim.diagnostic.goto_next, "Goto Prev Diagnostics")
+    nmap("gsl", vim.diagnostic.setloclist, "[g] [S]et[l]oclist")
 
     -- See `:help K` for why this keymap
     nmap("K", vim.lsp.buf.hover, "Hover Documentation")
     nmap("<C-h>", vim.lsp.buf.signature_help, "Signature Documentation")
-    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { buffer = bufnr, desc = "Signature Documentation" })
+    imap("<C-h>", vim.lsp.buf.signature_help, "Signature Documentation")
 
     -- Lesser used LSP functionality
     nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-    nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
     nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
     nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
     nmap("<leader>ws", vim.lsp.buf.workspace_symbol, "[W]orkspace [S]ymbol")
