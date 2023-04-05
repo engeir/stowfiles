0d24d4870b3206e99fdb5f475b9cefa6209d22f8
engeir <engeir@pm.me>
2023-04-05 16:11:35 +0200

    fix(revealjs): don't panic if no refs are present
    When running the script to generate references, it should be able to
    deal with no references as well.

--- bin/bin/revealjs-make-reflist.py
+++ bin/bin/revealjs-make-reflist.py
@@ -7,23 +7,26 @@ except IndexError:
     # If input comes from stdin, we are creating the final HTML code that goes into the
     # slide, therefore we start with ## References, and split up on every fourth
     # reference
-    print("## References\n")
     soup = BeautifulSoup(sys.stdin, features="lxml")
     div = soup.find("div", id="refs")
-    d_list = div.contents
-    # All even-indexed elements are newline characters
-    del d_list[::2]
-    new_page = False
-    i = 0
-    wc = 0
-    for element in d_list:
-        wc += len(element.get_text())
-        if wc > 1000 or (i > 0 and i % 4 == 0):
-            print("\n--\n")
-            i += 1
-            wc = 0
-        print('- <!-- .element: style="font-size:20pt" -->')
-        print(element)
+    if hasattr(div, "contents"):
+        print("---")
+        print("")
+        print("## References\n")
+        d_list = div.contents
+        # All even-indexed elements are newline characters
+        del d_list[::2]
+        new_page = False
+        i = 0
+        wc = 0
+        for element in d_list:
+            wc += len(element.get_text())
+            if wc > 1000 or (i > 0 and i % 4 == 0):
+                print("\n--\n")
+                i += 1
+                wc = 0
+            print('- <!-- .element: style="font-size:20pt" -->')
+            print(element)
 else:
     with open(file, "r") as fp:
         soup = BeautifulSoup(fp, features="lxml")
