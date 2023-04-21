0155d39c6e29b0e970ae7224b790030bdba382a3
engeir <engeir@pm.me>
2023-04-21 15:01:15 +0200

    chore: update nvim

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/neorg.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/neorg.lua
@@ -1,34 +1,35 @@
 return {
     "nvim-neorg/neorg",
-    enabled = false,
+    enabled = true,
+    build = ":Neorg sync-parsers",
+    ft = "norg",
+    cmd = "Neorg",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        "nvim-cmp",
+        "nvim-lua/plenary.nvim",
+    },
     opts = {
         load = {
-            ["core.defaults"] = {},
-            ["core.norg.dirman"] = {
+            ["core.defaults"] = {}, -- Loads default behaviour
+            ["core.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
+            ["core.concealer"] = { config = { icon_preset = "diamond" } },
+            ["core.dirman"] = { -- Manages Neorg workspaces
                 config = {
                     workspaces = {
+                        notes = "~/notes",
                         todo = "~/projects/todo",
                     },
                 },
             },
-            ["core.norg.concealer"] = {
-                config = { -- Note that this table is optional and doesn't need to be provided
-                    -- Configuration here
-                },
-            },
-            ["core.norg.completion"] = {
+            ["core.integrations.nvim-cmp"] = {},
+            ["core.export"] = {},
+            ["core.keybinds"] = {
+                -- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
                 config = {
-                    engine = "nvim-cmp",
-                },
-            },
-            ["core.integrations.treesitter"] = {
-                config = { -- Note that this table is optional and doesn't need to be provided
-                    -- Configuration here
-                },
-            },
-            ["core.gtd.base"] = {
-                config = { -- Note that this table is optional and doesn't need to be provided
-                    workspace = "~/projects/todo",
+                    default_keybinds = true,
+                    neorg_leader = "<Leader><Leader>",
                 },
             },
         },
