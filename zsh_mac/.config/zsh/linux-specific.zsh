11e364a0add14cfd0888fe15e44fb6acaa72f5d7
engeir <engeir@pm.me>
2023-06-21 13:15:20 +0200

    fix(alias): mount alias for LaCie was missing device path

--- zsh_mac/.config/zsh/linux-specific.zsh
+++ zsh_mac/.config/zsh/linux-specific.zsh
@@ -12,7 +12,7 @@ alias cp='/usr/local/bin/cpg -g'
 alias ll="ls -ltrh"
 alias lsn="ls --color=auto */ | less"
 alias mv='/usr/local/bin/mvg -g'
-alias lcm="sudo mount /media/een023/LaCie"
+alias lcm="sudo mount /dev/sda2 /media/een023/LaCie"
 alias lcu="sudo umount /media/een023/LaCie"
 alias lcc="lsblk -e 1,7"
 alias open="xdg-open"
