3754ea3b361f8813dcbd1f97b74dedb878bbdfaf
Eirik Rolland Enger <eirroleng@gmail.com>
2023-09-04 22:09:07 +0200

    sketchybar(lua): can't get the new lua config to work

---
+++ sbarLua/.config/sketchybar/bar.lua
@@ -0,0 +1,15 @@
+local colors = require("colors")
+
+-- Equivalent to the --bar domain
+sbar.bar({
+  height = 45,
+  color = colors.bar.bg,
+  border_width = 2,
+  border_color = colors.bar.border,
+  shadow = false,
+  sticky = true,
+  padding_right = 10,
+  padding_left = 10,
+  y_offset = -8,
+  margin = -2,
+})
