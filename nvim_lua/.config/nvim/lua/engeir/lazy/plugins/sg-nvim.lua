0738954f95cacb598875e41102510a91b27140ed
engeir <engeir@pm.me>
2023-10-05 17:53:09 +0200

    feat(nvim): add plugins sg.nvim and vim-matchup

---
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/sg-nvim.lua
@@ -0,0 +1,8 @@
+return {
+    "sourcegraph/sg.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true,
+    event = { "BufReadPre", "BufNewFile" },
+    -- -- If you have a recent version of lazy.nvim, you don't need to add this!
+    -- build = "nvim -l build/init.lua",
+}
