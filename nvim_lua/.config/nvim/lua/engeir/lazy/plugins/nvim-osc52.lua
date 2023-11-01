6f1a73eee3d7d12925936e0016f1c3e964104769
engeir <engeir@pm.me>
2023-11-01 17:17:00 +0100

    chore(nvim/tmux): autocmd copy to osc52 from + register

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/nvim-osc52.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/nvim-osc52.lua
@@ -6,5 +6,11 @@ return {
         vim.keymap.set("n", "<leader>y", require("osc52").copy_operator, { expr = true })
         vim.keymap.set("n", "<leader>yy", "<leader>y_", { remap = true })
         vim.keymap.set("v", "<leader>y", require("osc52").copy_visual)
+        local function copy()
+            if vim.v.event.operator == "y" and vim.v.event.regname == "+" then
+                require("osc52").copy_register("+")
+            end
+        end
+        vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
     end,
 }
