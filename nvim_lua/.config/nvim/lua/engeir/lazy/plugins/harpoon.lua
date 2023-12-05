7db5957de13b70db200680d7a6e0c38501b83234
engeir <engeir@pm.me>
2023-12-05 08:48:13 +0100

    feat(nvim): update to harpoon v2

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/harpoon.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/harpoon.lua
@@ -1,38 +1,57 @@
 return {
     "ThePrimeagen/harpoon",
-    opts = {
-        menu = {
-            width = math.floor(vim.api.nvim_win_get_width(0) * 0.5),
-        },
-    },
-    keys = {
-        {
-            "<leader>hf",
-            function()
-                require("harpoon.ui").toggle_quick_menu()
-            end,
-            desc = "[H]arpoon [F]iles",
-        },
-        {
-            "<leader>hm",
-            function()
-                require("harpoon.mark").add_file()
-            end,
-            desc = "[H]arpoon [M]ark",
-        },
-        {
-            "<leader>hn",
-            function()
-                require("harpoon.ui").nav_next()
-            end,
-            desc = "[H]arpoon [N]ext",
-        },
-        {
-            "<leader>hN",
-            function()
-                require("harpoon.ui").nav_prev()
-            end,
-            desc = "[H]arpoon [P]rev",
-        },
-    },
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local opts = {
+            settings = { save_on_toggle = true },
+            menu = {
+                width = math.floor(vim.api.nvim_win_get_width(0) * 0.5),
+            },
+        }
+        local harpoon = require("harpoon")
+        harpoon:setup(opts)
+        vim.keymap.set("n", "<leader>hm", function()
+            harpoon:list():append()
+        end)
+        vim.keymap.set("n", "<leader>hf", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end)
+        local HarpoonList = harpoon:list()
+        function HarpoonList:next()
+            self._index = self._index + 1
+            if self._index > #self.items then
+                self._index = 1
+            end
+
+            self:select(self._index)
+        end
+
+        function HarpoonList:prev()
+            self._index = self._index - 1
+            if self._index < 1 then
+                self._index = #self.items
+            end
+
+            self:select(self._index)
+        end
+        vim.keymap.set("n", "<leader>hn", function()
+            HarpoonList:next()
+        end)
+        vim.keymap.set("n", "<leader>hN", function()
+            HarpoonList:prev()
+        end)
+        vim.keymap.set("n", "<leader>1", function()
+            harpoon:list():select(1)
+        end)
+        vim.keymap.set("n", "<leader>2", function()
+            harpoon:list():select(2)
+        end)
+        vim.keymap.set("n", "<leader>3", function()
+            harpoon:list():select(3)
+        end)
+        vim.keymap.set("n", "<leader>4", function()
+            harpoon:list():select(4)
+        end)
+    end,
 }
