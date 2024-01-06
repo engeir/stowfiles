e45540a98416919aecf46179cbb336319f0e25ac
engeir <engeir@pm.me>
2024-01-06 02:14:26 +0100

    fix(nvim): properly setup obsidian vault

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/obsidian.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/obsidian.lua
@@ -2,14 +2,13 @@ return {
     "epwalsh/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
     lazy = true,
-    ft = "markdown",
+    cmd = { "ObsidianQuickSwitch" }, -- "ObsidianWorkspace"
+    -- ft = "markdown",
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-    --   "BufReadPre path/to/my-vault/**.md",
-    --   "BufNewFile path/to/my-vault/**.md",
-    -- },
+    event = {
+        "BufReadPre " .. vim.fn.expand("~") .. "/.local/share/obsidian-vault/**.md",
+        "BufNewFile " .. vim.fn.expand("~") .. "/.local/share/obsidian-vault/**.md",
+    },
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
@@ -17,12 +16,12 @@ return {
         workspaces = {
             {
                 name = "personal",
-                path = "~/vaults/personal",
+                path = "~/.local/share/obsidian-vault",
             },
-            {
-                name = "work",
-                path = "~/vaults/work",
-            },
+            -- {
+            --     name = "work",
+            --     path = "~/vaults/work",
+            -- },
         },
     },
 }
