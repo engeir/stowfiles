4892da77a83cd501fa0b56cbc1d1e1a808d21a64
engeir <engeir@pm.me>
2024-01-10 22:18:20 +0100

    style(sbar): format lua
    feat(sbar): add percentage to sketchybar volume item

--- sbarLua/.config/sketchybar/default.lua
+++ sbarLua/.config/sketchybar/default.lua
@@ -3,43 +3,42 @@ local colors = require("colors")
 
 -- Equivalent to the --default domain
 sbar.default({
-  updates = "when_shown",
-  icon = {
-    font = {
-      family = settings.font,
-      style = "Bold",
-      size = 11.0
+    updates = "when_shown",
+    icon = {
+        font = {
+            family = settings.font,
+            style = "Bold",
+            size = 11.0,
+        },
+        color = colors.white,
+        padding_left = settings.paddings,
+        padding_right = settings.paddings,
     },
-    color = colors.white,
-    padding_left = settings.paddings,
-    padding_right = settings.paddings,
-  },
-  label = {
-    font = {
-      family = settings.font,
-      style = "Semibold",
-      size = 11.0
+    label = {
+        font = {
+            family = settings.font,
+            style = "Semibold",
+            size = 11.0,
+        },
+        color = colors.white,
+        padding_left = settings.paddings,
+        padding_right = settings.paddings,
     },
-    color = colors.white,
-    padding_left = settings.paddings,
-    padding_right = settings.paddings,
-  },
-  background = {
-    height = 20,
-    corner_radius = 9,
-    border_width = 1,
-  },
-  popup = {
     background = {
-      border_width = 2,
-      corner_radius = 9,
-      border_color = colors.popup.border,
-      color = colors.popup.bg,
-      shadow = { drawing = true },
+        height = 20,
+        corner_radius = 9,
+        border_width = 1,
     },
-    blur_radius = 10,
-  },
-  padding_left = 1,
-  padding_right = 1
+    popup = {
+        background = {
+            border_width = 2,
+            corner_radius = 9,
+            border_color = colors.popup.border,
+            color = colors.popup.bg,
+            shadow = { drawing = true },
+        },
+        blur_radius = 10,
+    },
+    padding_left = 1,
+    padding_right = 1,
 })
-
--- sbarLua/.config/sketchybar/items/apple.lua
+++ sbarLua/.config/sketchybar/items/apple.lua
@@ -4,31 +4,31 @@ local colors = require("colors")
 local popup_toggle = "sketchybar --set $NAME popup.drawing=toggle"
 
 local apple_logo = sbar.add("item", {
-  padding_right = 5,
-  click_script = popup_toggle,
-  icon = {
-    string = icons.apple,
-    font = {
-      style = "Black",
-      size = 14.0,
+    padding_right = 5,
+    click_script = popup_toggle,
+    icon = {
+        string = icons.apple,
+        font = {
+            style = "Black",
+            size = 14.0,
+        },
+        color = colors.green,
     },
-    color = colors.green,
-  },
-  label = {
-    drawing = false,
-  },
-  popup = {
-    height=25
-  }
+    label = {
+        drawing = false,
+    },
+    popup = {
+        height = 25,
+    },
 })
 
 local apple_prefs = sbar.add("item", {
-  position = "popup." .. apple_logo.name,
-  icon = icons.preferences,
-  label = "Preferences",
+    position = "popup." .. apple_logo.name,
+    icon = icons.preferences,
+    label = "Preferences",
 })
 
 apple_prefs:subscribe("mouse.clicked", function(_)
-  os.execute("open -a 'System Preferences'")
-  apple_logo:set({ popup = { drawing = false } } )
+    os.execute("open -a 'System Preferences'")
+    apple_logo:set({ popup = { drawing = false } })
 end)
