8720ff6f3db1ce69410a3c6a13ef9423aef8dfa1
engeir <engeir@pm.me>
2024-01-26 09:03:33 +0100

    chore(fish): update commands

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+    atuin init fish | source
+    set -g fish_key_bindings fish_vi_key_bindings
+    set fish_cursor_default block
+    set fish_cursor_insert line
+    set fish_cursor_replace_one underscore
+    set fish_cursor_visual line
 end
