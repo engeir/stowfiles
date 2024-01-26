2759c0290c45d3a3b7f71f01c300686bd5ad2cce
engeir <engeir@pm.me>
2024-01-26 09:36:25 +0100

    chore(mise): activate and setup mise

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
     atuin init fish | source
+    mise activate fish | source
     set -g fish_key_bindings fish_vi_key_bindings
     set fish_cursor_default block
     set fish_cursor_insert line
