2495f40a1f8719c8a755b8065eb1d6ca3edc5b56
engeir <engeir@pm.me>
2024-04-23 08:23:14 +0200

    chore(fish): add ubi and zathura aliases
    Signed-off-by: engeir <engeir@pm.me>

---
+++ fish/.config/fish/functions/ubi.fish
@@ -0,0 +1,3 @@
+function ubi --description 'alias ubi=ubi -i /home/een023/.local/ubi-bin'
+ command ubi -i /home/een023/.local/ubi-bin $argv
+end
---
+++ fish/.config/fish/functions/zathura.fish
@@ -0,0 +1,3 @@
+function zathura --wraps='tabbed -c -r 2 zathura -e id' --description 'alias zathura=tabbed -c -r 2 zathura -e id'
+  tabbed -c -r 2 zathura -e id $argv
+end
