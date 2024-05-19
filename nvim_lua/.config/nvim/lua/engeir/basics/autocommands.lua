9ac54839f6791de0514bdabd555d2e1fd936976f
engeir <engeir@pm.me>
2024-05-19 11:12:01 +0200

    refactor(nvim): remove auto-hlsearch plugin for autocommand

--- nvim_lua/.config/nvim/lua/engeir/basics/autocommands.lua
+++ nvim_lua/.config/nvim/lua/engeir/basics/autocommands.lua
@@ -65,6 +65,18 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   end,
 })
 
+-- Auto turn-off the search highlight
+vim.api.nvim_create_autocmd("CursorMoved", {
+  group = vim.api.nvim_create_augroup("auto-hlsearch", { clear = true }),
+  callback = function()
+    if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
+      vim.schedule(function()
+        vim.cmd.nohlsearch()
+      end)
+    end
+  end,
+})
+
 -- Enable treesitter highlighting
 -- https://this-week-in-neovim.org/2023/Feb/20#core
 vim.api.nvim_create_autocmd("FileType", {
