7c1d3be4b55fe51a3fb82a6f92f796c8a630a8ff
engeir <engeir@pm.me>
2024-06-16 20:16:56 +0200

    style(typo): fix English grammar

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/ultimate-autopair-nvim.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/ultimate-autopair-nvim.lua
@@ -2,7 +2,7 @@ return {
   "altermo/ultimate-autopair.nvim",
   enabled = true,
   event = { "InsertEnter" },
-  branch = "v0.6", --recomended as each new version will have breaking changes
+  branch = "v0.6", --recommended as each new version will have breaking changes
   opts = {
     --Config goes here
   },
