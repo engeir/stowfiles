0ee3f5e5cad16063bb129492dac3f973912e2db3
engeir <engeir@pm.me>
2024-06-24 09:15:13 +0200

    feat(fzf): add in-file-search function with vim integration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+    fzf --fish | source
     atuin init fish | source
     mise activate fish | source
     zoxide init fish | source
--- zsh_mac/.config/zsh/functions.zsh
+++ zsh_mac/.config/zsh/functions.zsh
@@ -8,11 +8,11 @@ batdiff() {
 }
 
 brew() {
-  command brew "$@"
+    command brew "$@"
 
-  if [[ $* =~ "upgrade" ]] || [[ $* =~ "update" ]] || [[ $* =~ "outdated" ]]; then
-    sketchybar --trigger brew_update
-  fi
+    if [[ $* =~ "upgrade" ]] || [[ $* =~ "update" ]] || [[ $* =~ "outdated" ]]; then
+        sketchybar --trigger brew_update
+    fi
 }
 
 cht() {
@@ -156,6 +156,27 @@ nvp() {
     fi
 }
 compdef _whereis nvp
+
+# ripgrep->fzf->nvim [QUERY]
+r() { (
+    # https://junegunn.github.io/fzf/tips/ripgrep-integration/#wrap-up
+    RELOAD='reload:rg --column --color=always --smart-case {q} || :'
+    OPENER='if [[ $FZF_SELECT_COUNT -eq 0 ]]; then
+            nvim {1} +{2}     # No selection. Open the current line in NeoVim.
+          else
+            nvim +cw -q {+f}  # Build quickfix list for the selected items.
+          fi'
+    fzf </dev/null \
+        --disabled --ansi --multi \
+        --bind "start:$RELOAD" --bind "change:$RELOAD" \
+        --bind "enter:become:$OPENER" \
+        --bind "ctrl-o:execute:$OPENER" \
+        --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' \
+        --delimiter : \
+        --preview 'bat --style=full --color=always --highlight-line {2} {1}' \
+        --preview-window '~4,+{2}+4/3,<80(up)' \
+        --query "$*"
+); }
 
 wezterm-kill-pane() {
     wezterm cli kill-pane --pane-id "$(wezterm cli list | fzf | awk '{print $3}')"
