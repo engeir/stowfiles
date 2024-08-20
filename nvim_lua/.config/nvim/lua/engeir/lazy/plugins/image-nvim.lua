3e095551d96d6a542351d634484c60216e732187
engeir <engeir@pm.me>
2024-08-20 22:03:13 +0200

    chore(nvim): use image.nvim also on my arch system

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/image-nvim.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/image-nvim.lua
@@ -1,7 +1,7 @@
 return {
   {
     "3rd/image.nvim",
-    enabled = vim.loop.os_homedir() == "/home/een023",
+    enabled = (vim.loop.os_homedir() == "/home/een023" or vim.loop.os_homedir() == "/home/eirikre"),
     build = "luarocks --local --lua-version=5.1 install magick",
     event = "BufReadPre",
     init = function()
