dd994b75e4be0ad193969020395e193045843b05
engeir <engeir@pm.me>
2024-12-06 16:00:39 +0100

    style(bin): format some bash

--- bin/bin/git-search-file-for-string.sh
+++ bin/bin/git-search-file-for-string.sh
@@ -2,5 +2,5 @@
 
 export File="./src/ensemble_run_analysis/analysis/forcing_linearity.py"
 for Hash in $(git rev-list HEAD -- $File); do
-    git grep -i -F "c2c" $Hash:$File;
+    git grep -i -F "c2c" "$Hash:$File"
 done
