8a1d3a197fd54f7fc8de0ad4dedd733b92c5491e
engeir <engeir@pm.me>
2024-12-16 08:01:30 +0100

    chore(nvim): enable just plugin

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/just.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/just.lua
@@ -6,7 +6,7 @@ return {
   {
     {
       "al1-ce/just.nvim",
-      enabled = false,
+      enabled = true,
       ft = { "just" },
       -- An update seem to be in the works, where a more general `runme` command is used.
       -- commit = "c32dc02",
@@ -20,13 +20,19 @@ return {
         "JustCreateTemplate", -- Creates template justdfile with included "cheatsheet".
       },
       keys = {
-        { "<leader>jd", "<cmd>JustDefault<cr>", desc = "Just: Default" },
-        { "<leader>jb", "<cmd>JustBuild<cr>", desc = "Just: Build" },
-        { "<leader>jr", "<cmd>JustRun<cr>", desc = "Just: Run" },
-        { "<leader>jt", "<cmd>JustTest<cr>", desc = "Just: Test" },
+        { "<leader>jd", "<cmd>Just<cr>", desc = "Just: Default" },
+        { "<leader>jf", "<cmd>Just!<cr>", desc = "Just: Force Default" },
+        { "<leader>jb", "<cmd>Just build<cr>", desc = "Just: Build" },
+        { "<leader>jr", "<cmd>Just run<cr>", desc = "Just: Run" },
+        { "<leader>jt", "<cmd>Just test<cr>", desc = "Just: Test" },
         { "<leader>js", "<cmd>JustSelect<cr>", desc = "Just: Select" },
         { "<leader>jk", "<cmd>JustStop<cr>", desc = "Just: Kill" },
         { "<leader>jc", "<cmd>JustCreateTemplate<cr>", desc = "Just: Create" },
+        {
+          "<leader>jm",
+          "<cmd>JustCreateMakeTemplate<cr>",
+          desc = "Just: Create Make-like (changed files)",
+        },
       },
       dependencies = {
         "nvim-lua/plenary.nvim", -- async jobs
