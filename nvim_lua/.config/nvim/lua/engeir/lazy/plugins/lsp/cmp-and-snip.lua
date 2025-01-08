1d092c03745ad23a74deb8245d2c8b3182950f6c
engeir <engeir@pm.me>
2025-01-08 08:57:54 +0100

    fix(nvim)!: blink.cmp (0.10.0) updated their snippet definitions

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/lsp/cmp-and-snip.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/lsp/cmp-and-snip.lua
@@ -2,71 +2,44 @@ return {
   {
     "saghen/blink.cmp",
     lazy = false, -- lazy loading handled internally
-    -- optional: provides snippets for the snippet source
     dependencies = {
       "rafamadriz/friendly-snippets",
       { "L3MON4D3/LuaSnip", version = "v2.*" },
     },
-
-    -- use a release tag to download pre-built binaries
-    version = "v0.*",
-    -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
+    version = "v0.*", -- use a release tag to download pre-built binaries
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      -- 'default' for mappings similar to built-in completion
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-      -- see the "default configuration" section below for full documentation on how to define
-      -- your own keymap.
-      keymap = { preset = "default" },
-
+      keymap = {
+        preset = "default",
+        cmdline = {
+          preset = "enter",
+          ["<S-Tab>"] = { "select_prev", "fallback" },
+          ["<Tab>"] = { "select_next", "fallback" },
+          ["<CR>"] = { "fallback" },
+          ["<C-y>"] = { "accept", "fallback" },
+          -- disable a keymap from the preset
+          ["<C-e>"] = {},
+          ["<C-space>"] = {},
+        },
+      },
       appearance = {
-        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- Useful for when your theme doesn't support blink.cmp
-        -- will be removed in a future release
         use_nvim_cmp_as_default = false,
-        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = "mono",
       },
-
       completion = {
         accept = { auto_brackets = { enabled = true } },
         documentation = { auto_show = true },
       },
-      -- experimental auto-brackets support
-
-      -- experimental signature help support
       signature = { enabled = true },
-      snippets = {
-        expand = function(snippet)
-          require("luasnip").lsp_expand(snippet)
-        end,
-        active = function(filter)
-          if filter and filter.direction then
-            return require("luasnip").jumpable(filter.direction)
-          end
-          return require("luasnip").in_snippet()
-        end,
-        jump = function(direction)
-          require("luasnip").jump(direction)
-        end,
-      },
+      snippets = { preset = "luasnip" },
       sources = {
-        default = { "lsp", "path", "luasnip", "buffer" },
+        default = { "lsp", "path", "snippets", "buffer" },
       },
     },
-    -- allows extending the providers array elsewhere in your config
-    -- without having to redefine it
     opts_extend = { "sources.default" },
   },
-
-  -- Snippets
   {
     "L3MON4D3/LuaSnip",
     dependencies = {
