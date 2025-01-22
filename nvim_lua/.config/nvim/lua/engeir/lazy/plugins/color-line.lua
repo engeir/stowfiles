3b8e8bceb64d12a6d1bbb1c02313625048a13437
engeir <engeir@pm.me>
2025-01-22 11:09:28 +0100

    fix(nvim): update icon which was undefined
    Also moves the plugin into its own file. It didn't fit nicely into the
    colour-line plugin specs.

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/color-line.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/color-line.lua
@@ -37,33 +37,5 @@ return {
       })
     end,
   },
-  {
-    "folke/todo-comments.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TodoTrouble", "TodoTelescope" },
-    opts = {
-      keywords = {
-        FIXME = {
-          icon = " ",
-          color = "error", -- can be a hex color, or a named color (see below)
-          alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
-        },
-        TODO = { icon = " ", color = "#5888ef" }, -- 'info' is too dark
-        HACK = { icon = " ", color = "warning" },
-        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-        PERF = {
-          icon = " ",
-          color = "#ff9f00",
-          alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" },
-        },
-        NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-        URGE = { icon = "‼ ", color = "#c9ff19", alt = { "IMPORTANT" } },
-      },
-    },
-    keys = {
-      { "<leader>tq", "<cmd>TodoQuickFix<CR>", desc = "[T]odo[Q]uickFix" },
-      { "<leader>ft", "<cmd>TodoTelescope<CR>", desc = "[f]ind [T]odoTelescope" },
-    },
-  },
   { "mechatroner/rainbow_csv", ft = "csv" },
 }
