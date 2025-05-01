0298e1bc809c050bc7c340b63f4005f810118875
engeir <38856990+engeir@users.noreply.github.com>
2025-05-01 10:37:46 +0200

    fix(yazi): update after a break in the git plugin

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,8 +1,6 @@
 Status:children_add(function()
   local h = cx.active.current.hovered
-  if h == nil or ya.target_family() ~= "unix" then
-    return ui.Line({})
-  end
+  if h == nil or ya.target_family() ~= "unix" then return ui.Line({}) end
 
   return ui.Line({
     ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
@@ -13,10 +11,10 @@ Status:children_add(function()
 end, 500, Status.RIGHT)
 
 -- Git mode view
-THEME.git = THEME.git or {}
-THEME.git = THEME.git or {}
-THEME.git.modified_sign = "M"
-THEME.git.deleted_sign = "D"
-THEME.git.modified = ui.Style():fg("blue")
-THEME.git.deleted = ui.Style():fg("red"):bold()
+th.git = th.git or {}
+th.git = th.git or {}
+th.git.modified_sign = "M"
+th.git.deleted_sign = "D"
+th.git.modified = ui.Style():fg("blue")
+th.git.deleted = ui.Style():fg("red"):bold()
 require("git"):setup()
