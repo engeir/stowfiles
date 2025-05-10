dbaf9ffd11daf06f7b2e0c5203e7bf883ebcabee
engeir <38856990+engeir@users.noreply.github.com>
2025-05-10 16:31:50 +0200

    feat(nvim): add mason tool installer

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/lsp/mason.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/lsp/mason.lua
@@ -7,6 +7,40 @@ return {
   dependencies = {
     "mason-org/mason-lspconfig.nvim",
     version = "1.*",
+    {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      opts = {
+        auto_update = true,
+        ensure_installed = {
+          { "bash-language-server", auto_update = true },
+          "beautysh",
+          "harper-ls",
+          "html-lsp",
+          "jedi-language-server",
+          "json-lsp",
+          "jsonlint",
+          "lua-language-server",
+          "markdownlint",
+          "mypy",
+          "ruff",
+          "shellcheck",
+          "shellharden",
+          "sourcery",
+          "stylua",
+          "taplo",
+          "texlab",
+          "tinymist",
+          "typstfmt",
+          "yaml-language-server",
+          "yamlfmt",
+        },
+        integrations = {
+          ["mason-lspconfig"] = true,
+          ["mason-null-ls"] = false,
+          ["mason-nvim-dap"] = false,
+        },
+      },
+    },
   },
   config = function()
     local mason = require("mason")
