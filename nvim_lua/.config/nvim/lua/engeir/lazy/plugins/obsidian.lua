1b57d849dc869af85b796485a6baf0d5fb7ac0a9
Eirik Enger <eirik.enger@nhn.no>
2025-06-03 08:22:36 +0200

    feat(nvim): add completion support in obsidian

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/obsidian.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/obsidian.lua
@@ -3,11 +3,15 @@ return {
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
   cmd = { "ObsidianQuickSwitch" }, -- "ObsidianWorkspace"
+  keys = {
+    { "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>" },
+    { "<leader>on", "<cmd>ObsidianNew<CR>" },
+  },
   -- ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   event = {
-    "BufReadPre " .. vim.fn.expand("~") .. "/.local/share/obsidian-vault/**.md",
-    "BufNewFile " .. vim.fn.expand("~") .. "/.local/share/obsidian-vault/**.md",
+    "BufReadPre " .. vim.fn.expand("~") .. "/.local/share/obsidian-vault/Work/**.md",
+    "BufNewFile " .. vim.fn.expand("~") .. "/.local/share/obsidian-vault/Work/**.md",
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
@@ -23,6 +27,15 @@ return {
       --     name = "work",
       --     path = "~/vaults/work",
       -- },
+    },
+    -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+    completion = {
+      -- Enables completion using nvim_cmp
+      nvim_cmp = false,
+      -- Enables completion using blink.cmp
+      blink = true,
+      -- Trigger completion at 2 chars.
+      min_chars = 2,
     },
   },
 }
