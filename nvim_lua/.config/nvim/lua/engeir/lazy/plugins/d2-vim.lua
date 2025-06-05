71b9a9ec1f99f122804e9c0df5a44a043bb2e906
Eirik Enger <eirik.enger@nhn.no>
2025-06-05 08:45:48 +0200

    feat(nvim): add d2 highlighting and tree-sitter syntax

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/d2-vim.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/d2-vim.lua
@@ -1,5 +1,12 @@
 return {
-  "terrastruct/d2-vim",
-  enabled = false,
-  ft = { "d2" },
+  {
+    "terrastruct/d2-vim",
+    enabled = true,
+    ft = { "d2" },
+  },
+  {
+    "ravsii/tree-sitter-d2",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    build = "make nvim-install",
+  },
 }
