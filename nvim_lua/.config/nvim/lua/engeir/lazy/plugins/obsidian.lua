205de8c523c64aca1384c66395acd50235b5cd73
Eirik Enger <eirik.enger@nhn.no>
2025-06-05 08:44:18 +0200

    feat(nvim): make Obsidian plugin play better with the app

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/obsidian.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/obsidian.lua
@@ -20,8 +20,8 @@ return {
     ui = { enable = false },
     workspaces = {
       {
-        name = "personal",
-        path = "~/.local/share/obsidian-vault",
+        name = "work",
+        path = "~/.local/share/obsidian-vault/Work",
       },
       -- {
       --     name = "work",
@@ -37,5 +37,34 @@ return {
       -- Trigger completion at 2 chars.
       min_chars = 2,
     },
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'.
+      -- You may have as many periods in the note ID as you'd like—the ".md" will be added automatically
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.date("%Y-%m-%dT%H-%M-%S")) .. "-" .. suffix
+    end,
+
+    -- Optional, customize how note file names are generated given the ID, target directory, and title.
+    ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+    ---@return string|obsidian.Path The full path to the new note.
+    note_path_func = function(spec)
+      -- This is equivalent to the default behavior.
+      local path = spec.dir / tostring(spec.title)
+      return path:with_suffix(".md")
+    end,
   },
 }
