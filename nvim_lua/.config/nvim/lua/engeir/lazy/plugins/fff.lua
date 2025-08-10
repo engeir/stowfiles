b6d1fb6b69214c142d53931c89b5320413f603b8
engeir <38856990+engeir@users.noreply.github.com>
2025-08-10 21:59:52 +0200

    style(nvim): format lua code
    Signed-off-by: engeir <38856990+engeir@users.noreply.github.com>

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/fff.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/fff.lua
@@ -1,20 +1,16 @@
 return {
   "dmtrKovalenko/fff.nvim",
   build = "cargo +nightly build --release",
-  opts = {prompt="➜ "},
+  opts = { prompt = "➜ " },
   keys = {
     {
       "<leader>ff",
-      function()
-        require("fff").find_files()
-      end,
+      function() require("fff").find_files() end,
       desc = "Find Files",
     },
     {
       "<leader>fg",
-      function()
-        require("fff").find_in_git_root()
-      end,
+      function() require("fff").find_in_git_root() end,
       desc = "Find Git Files",
     },
   },
