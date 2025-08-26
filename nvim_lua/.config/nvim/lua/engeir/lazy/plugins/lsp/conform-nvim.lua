74b02c6884aef5e36d801f0c13439af4c3b527f1
Eirik Rolland Enger <eirik.enger@nhn.no>
2025-08-26 22:03:35 +0200

    chore(nvim): remove the deprecated typstfmt

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/lsp/conform-nvim.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/lsp/conform-nvim.lua
@@ -72,7 +72,7 @@ return {
         toml = { "dprint", "taplo", stop_after_first = false },
         typescript = { "prettierd", "dprint", "prettier", stop_after_first = true },
         typescriptreact = { "prettierd", "dprint", "prettier", stop_after_first = true },
-        typst = { "typstfmt", "typstyle", lsp_format = "first" },
+        typst = { "typstyle", lsp_format = "first" },
         vim = { "vimfmt", "injected" },
         yaml = function(bufnr)
           return { first(bufnr, "yamlfmt", "prettierd", "prettier"), "injected" }
