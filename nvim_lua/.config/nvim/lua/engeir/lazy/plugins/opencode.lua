c9ffbb17bb016116777c8ccfde78fcbed3c89df7
engeir <38856990+engeir@users.noreply.github.com>
2025-09-29 21:59:40 +0200

    feat(nvim): [plugin] add embedded opencode via opencode.nvim
    Signed-off-by: engeir <38856990+engeir@users.noreply.github.com>

---
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/opencode.lua
@@ -0,0 +1,10 @@
+return {
+  "sudo-tee/opencode.nvim",
+  opts = {},
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "saghen/blink.cmp",
+    "folke/snacks.nvim",
+    -- 'nvim_mini/mini.nvim',
+  },
+}
