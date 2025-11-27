65716950454e9275497deaa7e3396e29dbe148ee
engeir <38856990+engeir@users.noreply.github.com>
2025-11-27 19:15:40 +0100

    feat(mango): auto attach/detach monitors with shikane
    Signed-off-by: engeir <38856990+engeir@users.noreply.github.com>

--- mango/.config/mango/autostart.sh
+++ mango/.config/mango/autostart.sh
@@ -10,43 +10,46 @@ has() {
 
 set +e
 
-has dbus-update-activation-environment
-has dunst
-has swaybg
-has waybar
-has clipcatd
-has xrdb
-has fcitx5
-has blueman-applet
-has nm-applet
-has swayosd-server
-
 # obs
+has dbus-update-activation-environment
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots >/dev/null 2>&1
 
+# monitor auto attach
+has shikane
+pgrep -x shikane >/dev/null 2>&1 || shikane >/dev/null 2>&1 &
+
 # notify
+has dunst
 pgrep -x dunst >/dev/null || dunst -conf "$HOME/.config/dunst/dunstrc" >/dev/null 2>&1 &
 
 # wallpaper
+has swaybg
 swaybg -m fill -i ~/.cache/wallpaper/wallpaper.jpg >/dev/null 2>&1 &
 
 # top bar
-waybar -c ~/.config/mango/waybar/config.jsonc -s ~/.config/mango/waybar/style.css >/dev/null 2>&1 &
+has waybar
+~/.config/mango/scripts/hide_waybar_mango.sh
 
-clipcatd >/dev/null 2>&1 &
+has clipcatd
+pgrep -x clipcatd || clipcatd >/dev/null 2>&1 &
 
 # xwayland dpi scale
+has xrdb
 echo "Xft.dpi: 140" | xrdb -merge # DPI scaling
 # xrdb merge ~/.Xresources >/dev/null 2>&1
 
 # ime input
+has fcitx5
 fcitx5 --replace -d >/dev/null 2>&1 &
 
 # bluetooth
-blueman-applet >/dev/null 2>&1 &
+has blueman-applet
+pgrep -x blueman-applet || blueman-applet >/dev/null 2>&1 &
 
 # network
-nm-applet >/dev/null 2>&1 &
+has nm-applet
+pgrep -x nm-applet || nm-applet >/dev/null 2>&1 &
 
 # change light value and volume value by swayosd-client in keybind
-swayosd-server >/dev/null 2>&1 &
+has swayosd-server
+pgrep -x swayosd-server || swayosd-server >/dev/null 2>&1 &
--- mango/.config/mango/scripts/hide_waybar_mango.sh
+++ mango/.config/mango/scripts/hide_waybar_mango.sh
@@ -5,5 +5,5 @@ startd=$(pgrep waybar)
 if [ -n "$startd" ]; then
 	pkill waybar
 else
-	waybar -c ~/.config/mango/waybar/config.jsonc -s ~/.config/mango/waybar/style.css
+	waybar -c ~/.config/mango/waybar/config.jsonc -s ~/.config/mango/waybar/style.css >/dev/null 2>&1 &
 fi
