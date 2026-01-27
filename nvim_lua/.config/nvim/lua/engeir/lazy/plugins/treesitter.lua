6499544d0b7dc13f5f59a0ae793ff4d4e33701f9
engeir <38856990+engeir@users.noreply.github.com>
2026-01-27 20:13:13 +0100

    feat(mise,nvim): add TS highlighting
    This follow the
    [mise-neovim-cookbook](https://mise.jdx.dev/mise-cookbook/neovim.html)
    
    Signed-off-by: engeir <38856990+engeir@users.noreply.github.com>

--- nvim_lua/.config/nvim/lua/engeir/lazy/plugins/treesitter.lua
+++ nvim_lua/.config/nvim/lua/engeir/lazy/plugins/treesitter.lua
@@ -5,6 +5,13 @@ return {
     branch = "master",
     lazy = false,
     priority = 200,
+    init = function()
+      require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
+        local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+        local filename = vim.fn.fnamemodify(filepath, ":t")
+        return string.match(filename, ".*mise.*%.toml$") ~= nil
+      end, { force = true, all = false })
+    end,
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = {
