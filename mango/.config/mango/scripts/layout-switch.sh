c6c32b749f3998c8dca9ece8d18531563c69deb1
engeir <38856990+engeir@users.noreply.github.com>
2026-05-21 21:39:24 +0200

    fix(mango): update layout names
    Signed-off-by: engeir <38856990+engeir@users.noreply.github.com>

--- mango/.config/mango/scripts/layout-switch.sh
+++ mango/.config/mango/scripts/layout-switch.sh
@@ -7,18 +7,20 @@ fi
 
 # Create a mapping of short codes to descriptions
 declare -A layout_map=(
-    ["CT"]="center tile: Place the main tile in the centre"
-    ["G"]="grid: Equal size to all tiles, filling the screen as much as possible"
-    ["K"]="deck: Show the main tile and a second tile, where all other windows are stacked"
-    ["M"]="monocle: Focus only one tile across the whole tag"
-    ["RT"]="right tile: Master tile to the right, new tiles stack vertically to the left"
-    ["S"]="scroller: Scroll tiles horizontally"
     ["T"]="tile: Master tile to the left, new tiles stack vertically to the right"
-    ["TG"]="tile+grid: Like 'tile' for few windos, but like 'grid' as windows are added."
-    ["VG"]="vertical grid: Same as 'grid', but filling in the vertical direction first"
-    ["VK"]="vertical deck: Show the main tile and a second tile, where all other windows are stacked"
+    ["S"]="scroller: Scroll tiles horizontally"
+    ["G"]="grid: Equal size to all tiles, filling the screen as much as possible"
+    ["M"]="monocle: Focus only one tile across the whole tag"
+    ["K"]="deck: Show the main tile and a second tile, where all other windows are stacked"
+    ["CT"]="center tile: Place the main tile in the centre"
+    ["RT"]="right tile: Master tile to the right, new tiles stack vertically to the left"
     ["VS"]="vertical scroller: Scroll tiles vertically"
     ["VT"]="vertical tile: Master tile above, new tiles stack horizontally below"
+    ["VG"]="vertical grid: Same as 'grid', but filling in the vertical direction first"
+    ["VK"]="vertical deck: Show the main tile and a second tile, where all other windows are stacked"
+    ["DW"]="dwindle: new windows are created in the bottom right corner"
+    ["F"]="fair: Like 'tile' for few windos, and similar to 'grid' as windows are added."
+    ["VF"]="vetical fair: Like 'tile' for few windos, but like 'grid' as windows are added."
 )
 
 # Get available layouts from mmsg
