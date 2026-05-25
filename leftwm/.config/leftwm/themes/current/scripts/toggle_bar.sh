70a94b8c70c005fd8fb60d418e923e2e7db8ac7e
engeir <38856990+engeir@users.noreply.github.com>
2026-05-25 18:25:12 +0200

    chore(leftwm): use current theme name directly
    Signed-off-by: engeir <38856990+engeir@users.noreply.github.com>

--- leftwm/.config/leftwm/themes/current/scripts/toggle_bar.sh
+++ leftwm/.config/leftwm/themes/current/scripts/toggle_bar.sh
@@ -2,5 +2,5 @@
 if pgrep -x tint2 >/dev/null; then
     pkill tint2
 else
-    tint2 -c "$HOME/.config/leftwm/themes/gruvbox/tint2rc" &
+    tint2 -c "$HOME/.config/leftwm/themes/current/tint2rc" &
 fi
